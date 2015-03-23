db3f2e08fb84afb505711196b67a4423d97e8e4f
unknown <Phung.Tran@PhungTran-PC.lanworks.local>
2015-03-23 16:44:00 +0700

    final

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -13,33 +13,15 @@ pollsApp.controller('BlCtrl', ['$scope', 'PostsService', 'cfpLoadingBar', functi
         });
     }
 
-    function mask(visible) {
-        return visible ? true : false;
-    }
-
     init();
 
 }]);
 
 pollsApp.controller('ContactCtrl', ['$scope', 'PostsService', 'cfpLoadingBar', function ($scope, PostsService, cfpLoadingBar) {
     
-    function init() {
-        var data = {
-            'data1': 1,
-            'data2': 2
-        };
-        
-        cfpLoadingBar.start();
-        PostsService.postData(data).then(function(data) {
-            $scope.posts = data.posts;
-            cfpLoadingBar.complete()
-        });
-    }
-
-    function mask(visible) {
-        return visible ? true : false;
-    }
-
-    init();
-
+    
+    $scope.insertContact = function() {
+        console.log('Here');
+    };
+    
 }]);
